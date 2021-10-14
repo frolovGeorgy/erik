756ebacc767834d7e348e7903dd1cc07dd815cbf
Frolov Georgy <garry96.96@yandex.ru>
2021-10-14 17:10:17 +0300

    removed comment and renamed arr

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,14 +1,13 @@
 from random import randint
 
-sas = [randint(1, 10) for _ in range(10)]
-len_sas = len(sas)
+arr = [randint(1, 10) for _ in range(10)]
+len_arr = len(arr)
 
-print(f'Список до сортировки: {sas}')
+print(f'Список до сортировки: {arr}')
 
-for moving_num_index in range(len_sas):
-    for compared_num_index in range(moving_num_index, len_sas):
-        if sas[moving_num_index] > sas[compared_num_index]:
-            sas[compared_num_index], sas[moving_num_index] = sas[moving_num_index], sas[compared_num_index]
-    # print(f'Список после {moving_num_index + 1} сортировки: {sas}')
+for moving_num_index in range(len_arr):
+    for compared_num_index in range(moving_num_index, len_arr):
+        if arr[moving_num_index] > arr[compared_num_index]:
+            arr[compared_num_index], arr[moving_num_index] = arr[moving_num_index], arr[compared_num_index]
 
-print(f'Список после сортировки: {sas}')
+print(f'Список после сортировки: {arr}')
